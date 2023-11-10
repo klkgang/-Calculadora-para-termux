cfbc33725892589f378d65ba9f253428fec2df3b
Denis Cortez <139710771+klkgang@users.noreply.github.com>
2023-11-10 03:38:00 +0000

    0.01

--- calculadora.py
+++ calculadora.py
@@ -29,246 +29,50 @@ def banner_principal():
 
 
 #funcion para sumar
-def sumar():
-    limpiar_pantalla()
-    print(Fore.CYAN + "=== Opciones ===")
+def  calculadora():
+    while True:
+        try:
+            limpiar_pantalla()
+            banner_principal()
+            
+            print()
+            print(Fore.YELLOW + "Escriba 'help' para ayuda")
+            print()
+            print(Fore.GREEN + "Escriba 'exit' para salir")
+            print()
+            operacion = input(Fore.GREEN + "Ingrese su operacion:")
 
-    print()
-    print (Fore.BLUE + "1. Sumar numeros enteros")
-    print (Fore.BLUE + "2. Sumar nuneros decimales")
-    print (Fore.BLUE + "3. Volver a el menu principal")
-
-    print()
-    opcion = int(input(Fore.CYAN + "Seleccione una opcion: "))
-
-    if opcion == 1:
-        limpiar_pantalla()
-        numero1 = int(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
-
-        print()
-        numero2 = int(input(Fore.BLUE + "Mas: "))
-        resultado = numero1 + numero2
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "+", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        sumar()
-
-    elif opcion == 2:
-        limpiar_pantalla()
-        numero1= float(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
-
-        print()
-        numero2 = float(input(Fore.BLUE + "Mas: "))
-        resultado = round(numero1 + numero2, 2)
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "+", str(numero2), "=",str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        sumar()
-
-    elif opcion == 3:
-        menu_princiapal()
-
-    else:
-        print (Fore.RED + "Opcion no valida")
-        input(Fore.GREEN + "Presione Enter para continuar ")
-        sumar()
-#funcion para restar
-def restar():
-    limpiar_pantalla()
-    print (Fore.CYAN + "=== Opciones ===")
-
-    print()
-    print (Fore.BLUE + "1. Restar numeros enteros")
-    print (Fore.BLUE + "2. Restar numeros decimales")
-    print (Fore.BLUE + "3. Volver a el menu principal")
-
-    print()
-    opcion = int(input(Fore.CYAN + "Seleccione una opcion "))
-
-    if opcion == 1:
-        limpiar_pantalla()
-        numero1 = int(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE +"Numero 1 =", str(numero1))
-
-        print()
-        numero2 = int(input(Fore.BLUE + "Menos: "))
-        resultado = numero1 - numero2
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "-", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        restar()
-
-    elif opcion == 2:
-        limpiar_pantalla()
-        numero1= float(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
-
-        print()
-        numero2 = float(input(Fore.BLUE + "Menos: "))
-        resultado = round(numero1 - numero2, 2)
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "-", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        restar()
-
-    elif opcion == 3:
-        menu_princiapal()
-
-    else:
-        print ("opcion no valida")
-        input("Presione Enter para continuar ")
-        restar()
-#funcion de dividir
-def dividir():
-    limpiar_pantalla()
-    print (Fore.CYAN + "=== Opciones ===")
-
-    print()
-    print (Fore.BLUE + "1. Dividir numeros enteros")
-    print (Fore.BLUE + "2. Dividir numeros decimales")
-    print (Fore.BLUE + "3. Volver a el menu principal")
-
-    print()
-    opcion = int(input(Fore.CYAN + "Seleccione una opcion: "))
-
-    if opcion == 1:
-        limpiar_pantalla()
-        numero1 = int(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
-
-        print()
-        numero2 = int(input("Dividido entre: "))
-        resultado = numero1 // numero2
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "÷", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        dividir()
-
-    elif opcion == 2:
-        limpiar_pantalla()
-        numero1= float(input("Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", numero1)
-
-        print()
-        numero2 = float(input(Fore.BLUE + "Dividido entre: "))
-        resultado = round(numero1 / numero2, 2)
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "÷", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        dividir()
-
-    elif opcion == 3:
-        menu_princiapal()
-
-    else:
-        print (Fore.RED + "opcion no valida")
-        input(Fore.YELLOW + "Presione Enter para continuar ")
-        dividir()
-#funcion de multiplicar
-def multiplicar():
-    limpiar_pantalla()
-    print (Fore.CYAN + "=== Opciones ===")
-
-    print()
-    print (Fore.BLUE + "1. Multiplicar numeros enteros")
-    print (Fore.BLUE + "2. Multiplicar numeros decimales")
-    print (Fore.BLUE + "3. Volver a el menu principal")
-
-    print()
-    opcion = int(input(Fore.CYAN + "Seleccione una opcion: "))
+            if operacion == 'exit':
+                sys.exit()
 
 
-    if opcion == 1:
-        limpiar_pantalla()
-        numero1 = int(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
+            elif operacion == "help":
+                with open('help.txt', 'r') as archivo:
+                    limpiar_pantalla()
+                    print(archivo.read())
 
-        print()
-        numero2 = int(input(Fore.BLUE + "Multiplicado por: "))
-        resultado = numero1 * numero2
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "x", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        multiplicar()
-
-    elif opcion == 2:
-        limpiar_pantalla()
-        numero1= float(input(Fore.BLUE + "Num1: "))
-        limpiar_pantalla()
-        print (Fore.BLUE + "Numero 1 =", str(numero1))
-
-        print()
-        numero2 = float(input(Fore.BLUE + "Multiplicado por: "))
-        resultado = round(numero1 * numero2, 2)
-        limpiar_pantalla()
-
-        print (Fore.BLUE + str(numero1), "x", str(numero2), "=", str(resultado))
-        input(Fore.YELLOW + "Presione Enter para continuar. ")
-        multiplicar()
-
-    elif opcion == 3:
-        menu_princiapal()
-
-    else:
-        print (Fore.RED + "opcion no valida")
-        input(Fore.YELLOW + "Presione Enter para continuar ")
-        multiplicar()
-#el menu principal
-def menu_princiapal():
-    limpiar_pantalla()
-    banner_principal()
-
-    print()
-    print (Fore.BLUE + "Opciones:")
-    print (Fore.BLUE + "1. Sumar")
-    print (Fore.BLUE + "2. Restar")
-    print (Fore.BLUE + "3. Dividir")
-    print (Fore.BLUE + "4. Multiplicar")
-    print (Fore.BLUE + "5. Salir")
-    print()
-    opcion= int(input(Fore.CYAN + "Seleccione una opcion: "))
-
-    if opcion == 1:
-        sumar()
-
-    elif opcion == 2:
-        restar()
-
-    elif opcion == 3:
-        dividir()
-
-    elif opcion == 4:
-        multiplicar()
-
-    elif opcion == 5:
-        print (Fore.RED + "Cerrando script.....")
-        input(Fore.YELLOW +"Presione enter para continuar")
-        sys.exit()
+                    print()
+                    input(Fore.RED + "Presione enter para continuar")
+                    continue
 
 
-    else:
-        print(Fore.RED + "Opcion no valida.")
-        input(Fore.YELLOW + "Presione Enter para continuar ")
-        limpiar_pantalla()
-        menu_princiapal()
+            resultado = eval(operacion)
+
+            if resultado:
+                limpiar_pantalla()
+                print(Fore.GREEN + "El resultado es: " + str(resultado))
+                print()
+                input(Fore.RED + "Presione enter para continuar")
+                continue
+
+        except Exception:
+            input(Fore.RED + "Operacion invalida, presione enter para continuar")
+            continue
+        
 
 
+    
+    
 
 if __name__ == "__main__":
-    menu_princiapal()
+    calculadora()
